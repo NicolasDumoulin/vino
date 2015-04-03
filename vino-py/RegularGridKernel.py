4f7474082b3806522a93b18ca386351e7af740d1
Nicolas Dumoulin <nicolas.dumoulin@irstea.fr>
2015-04-03 12:00:41 +0200

    ability to give the whole grid

--- vino-py/RegularGridKernel.py
+++ vino-py/RegularGridKernel.py
@@ -5,11 +5,16 @@ import h5py
 from hdf5common import HDF5Writer
 
 class RegularGridKernel:
-  def __init__(self, originCoords, dimensionsSteps, dimensionsExtents):
+  def __init__(self, originCoords, dimensionsSteps, dimensionsExtents=None):
     self.originCoords = originCoords
     self.dimensionsSteps = dimensionsSteps
     self.dimensionsExtents = dimensionsExtents
-    self.grid = np.full(dimensionsExtents, False, dtype='bool')
+    if (dimensionsExtents):
+      self.grid = np.full(dimensionsExtents, False, dtype='bool')
+    
+  def setGrid(self, grid):
+    self.dimensionsExtents = grid.shape
+    self.grid = grid
     
   def set(self, coords, value):
     self.grid[coords] = value
@@ -30,7 +35,6 @@ class RegularGridKernel:
     with HDF5Writer(filename) as w:
       w.writeMetadata([["name","foo"]], [["name","greatAlgo"]])
       coords_list = [[i,j] for i,row in enumerate(self.grid) for j,e in enumerate(row) if e]
-      print(float(len(coords_list))/self.grid.size)
       w.writeData(coords_list, {
         'origin' : self.originCoords,
         'steps' : self.dimensionsSteps,
