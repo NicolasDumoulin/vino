c81be95b40082ffdd1c2e5a4debe636f3f9f5016
Dumoulin Nicolas <nicolas.dumoulin@irstea.fr>
2016-06-22 15:49:08 +0200

    samples

---
+++ vino-py/buildSamples.py
@@ -0,0 +1,22 @@
+from BarGridKernel import BarGridKernel
+from hdf5common import HDF5Manager
+import re
+import METADATA
+
+def testRead():
+    hm = HDF5Manager([BarGridKernel])
+    grid = BarGridKernel.readPatrickSaintPierrebis('../samples/2D.txt')
+    myre = re.compile('^#(.*):(.*)$')
+    with open('dataessai.txt') as f:
+      for line in f:
+        if line.startswith('#'):
+          k,v = myre.match(line).groups()
+          grid.metadata[k.strip()]=v.strip()
+    hm.writeKernel(grid, 'test.h5')
+    grid2 = hm.readKernel('test.h5')
+#    print(grid2.metadata)
+#    print(metadata.category)
+    print(grid2.metadata[METADATA.category])
+
+if __name__ == "__main__":
+    testRead()
