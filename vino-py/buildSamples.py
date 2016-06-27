b8e3783f3d1501d7b2dc762774353d39408bd411
Sophie Martin <sophie.martin@irstea.fr>
2016-06-27 11:01:13 +0200

    Bugfix in findandsaveobject

--- vino-py/buildSamples.py
+++ vino-py/buildSamples.py
@@ -1,10 +1,11 @@
 from BarGridKernel import BarGridKernel
-from hdf5common import HDF5Manager
+from hdf5common import HDF5Manager,HDF5Writer
 import re
 import METADATA
 
 def testRead():
     hm = HDF5Manager([BarGridKernel])
+    
     grid = BarGridKernel.readPatrickSaintPierrebis('../samples/2D.txt')
     myre = re.compile('^#(.*):(.*)$')
     with open('../samples/2D_metadata.txt') as f:
@@ -17,6 +18,8 @@ def testRead():
 #    print(grid2.metadata)
 #    print(metadata.category)
     print(grid2.metadata[METADATA.category])
+    print(grid2.metadata[METADATA.results_submissiondate])
+
 
 if __name__ == "__main__":
     testRead()
