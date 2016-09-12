516064f3f826afb98f036f319dd82b4ffe20a36e
Dumoulin Nicolas <nicolas.dumoulin@irstea.fr>
2016-09-12 11:26:49 +0200

    first version of reset script

---
+++ vinosite/resetDB.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+BAK=db.sqlite3.$(date +%Y%m%d-%H%M%S).bak
+echo "database saved to $BAK"
+mv db.sqlite3 $BAK && \
+python manage.py makemigrations sharekernel && \
+python manage.py migrate && \
+echo "migration OK"
