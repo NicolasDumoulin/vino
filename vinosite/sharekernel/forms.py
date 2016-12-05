431c3310fbeedcde17dd76d5e35daa2f4405c60e
Dumoulin Nicolas <nicolas.dumoulin@irstea.fr>
2016-12-05 12:36:26 +0100

    new forms for viabilityProblem and metadata

--- vinosite/sharekernel/forms.py
+++ vinosite/sharekernel/forms.py
@@ -6,6 +6,7 @@ Created on Fri Sep 11 13:49:04 2015
 """
 
 from django import forms
+from models import ViabilityProblem
 
 class DocumentForm(forms.Form):
     docfile = forms.FileField(
@@ -15,3 +16,15 @@ class DocumentForm(forms.Form):
 
 class TrajForm(forms.Form):
     controlInput = forms.CharField(label = '(time,control value)',initial = 'essai')
+    
+class ViabilityProblemForm(forms.ModelForm):
+    class Meta:
+        model = ViabilityProblem
+        exclude = ['category']
+        
+class MetadataFromListForm(forms.Form):
+    def __init__(self, metadata, *args, **kwargs):
+        super(forms.Form, self).__init__(*args, **kwargs)
+        # now we add each metadata individually
+        for metadata in metadata:
+             self.fields[metadata] = forms.CharField(label=metadata)
