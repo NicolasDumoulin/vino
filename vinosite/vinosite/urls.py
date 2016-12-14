c1301829bdd2605e1394943b31ba1c5ec479f6d0
Dumoulin Nicolas <nicolas.dumoulin@irstea.fr>
2016-12-14 17:02:39 +0100

    automatic doc for django (available from admin backend, link "documentation" on top)

--- vinosite/vinosite/urls.py
+++ vinosite/vinosite/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('sharekernel.urls', namespace="sharekernel")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
