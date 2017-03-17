49eca3fbfe958fba63cc0cb20b9fac97d9c3ac50
Dumoulin Nicolas <nicolas.dumoulin@irstea.fr>
2017-03-17 12:27:11 +0100

    populate database with some kernels

--- vinosite/sharekernel/management/commands/populate_database.py
+++ vinosite/sharekernel/management/commands/populate_database.py
@@ -1,46 +1,67 @@
 from django.core.management.base import BaseCommand
-from sharekernel.models import ResultFormat, ViabilityProblem, Algorithm
+from sharekernel.models import ResultFormat, Parameters, ViabilityProblem, Algorithm, Results
+from django.core.files import File
 from django.core.exceptions import ValidationError
+from FileFormatLoader import Loader
+import re
+import METADATA
+from django.utils import timezone
+from hdf5common import HDF5Manager
+import tempfile
+from BarGridKernel import BarGridKernel
+from django.utils.text import slugify
+from sharekernel.views import findandsaveobject
+
+hdf5manager = HDF5Manager([BarGridKernel])
+
+def addKernel(kernel, file):
+    resultformat_md = {k:v for k,v in kernel.metadata.iteritems() if k.startswith("resultformat")}
+    resultformat = findandsaveobject(ResultFormat, {METADATA.resultformat_name:resultformat_md[METADATA.resultformat_name]}, add_metadata=resultformat_md)
+    problem_md = {k:v for k,v in kernel.metadata.iteritems() if k.startswith("viabilityproblem")}
+    problem = findandsaveobject(ViabilityProblem, {METADATA.title:problem_md[METADATA.title]}, add_metadata=problem_md)
+    parameters_md = {k:v for k,v in kernel.metadata.iteritems() if k.startswith("parameters")}
+    parameters = findandsaveobject(Parameters, parameters_md, foreignkeys={'viabilityproblem':problem})
+    algorithm_md = {k:v for k,v in kernel.metadata.iteritems() if k.startswith("algorithm")}
+    algorithm = findandsaveobject(Algorithm, {METADATA.algorithm_name:algorithm_md[METADATA.algorithm_name]}, add_metadata=algorithm_md)
+    tmpfile = tempfile.NamedTemporaryFile(prefix=slugify(kernel.metadata[METADATA.title]),suffix=".h5")
+    hdf5manager.writeKernel(kernel, tmpfile.name)
+    result = findandsaveobject(Results, kernel.metadata, foreignkeys={
+            "parameters": parameters,
+            "algorithm": algorithm,
+            "resultformat": resultformat
+        },fields={
+            "datafile": File(tmpfile),
+            "submissiondate": timezone.now()
+    })
+    return result
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for name, description, parameterlist in [
-            ['bars','Each file line contains a given number of integers ranging from 0 to PointNumberPerAxis corresponding to a segment. The result is the union of all these segments. If (4,6,46,57) is written on a line, that means that the problem is 3-dimensional and that points (4,6,46), (4,6,47), ... ,(4,6,57) belong to the result set. Moreover, all real-valued points whose sup-distance is smaller than PointSize/2 also belong to the result set. ColumnDescription corresponds to identity matrix if the file columns correspond to the order according to which variables appear in the model description. Again in dimension 3 for instance, (0,0,0) corresponds to MinimalValues (possibly interchanged according to ColumnDescription) and (PointNumberPerAxis,PointNumberPerAxis,PointNumberPerAxis) to MaximalValues.','MinimalValues,MaximalValues,PointNumberPerAxis,PointSize,ColumnDescription'],
-            ['kdtree','Each file line contains a given number of real numbers separated by a blank describing the coordinates of one point in the cube, then the minimum and maximum values on each axis describing the cube belonging to the approximated kernel.','ColumnDescription']
-            ]:
-            o = ResultFormat(name=name, description=description, parameterlist=parameterlist)
-            try:
-                o.full_clean()
-            except ValidationError as e:
-                self.stdout.write("Format '{}' can't be added because of these field's errors: {}".format(name,e))
-            o.save()
+        ResultFormat.objects.all().delete()
+        ViabilityProblem.objects.all().delete()
+        Algorithm.objects.all().delete()
+        Parameters.objects.all().delete()
+        Results.objects.all().delete()
+        # Now populating some kernels
+        loader = Loader()
+        myre = re.compile('^#(.*):(.*)$')
+        for lake in ['2D','2D_light']:
+            metadata = {}
+            with open('../samples/lake/'+lake+'_metadata.txt') as f:
+                for line in f:
+                    if line.startswith('#'):
+                        k, v = myre.match(line).groups()
+                        metadata[k.strip()] = v.strip()
+            f = '../samples/lake/'+lake+'.txt'
+            kernel = loader.load(f)
+            kernel.metadata.update(metadata)
+            addKernel(kernel, f)
+        
+        for f in ['../samples/lake/lake_Isa_R1.dat', '../samples/bilingual-viabilitree/Bilingual21TS05dil3.dat', '../samples/bilingual-viabilitree/bilingual21dil0control0.1ustep0.01WC.dat']:
+            kernel = loader.load(f)
+            addKernel(kernel, f)
+        
         for vp in [{
-            'category':None,
-            'title':'Lake eutrophication',
-            'issue':'Many lake have experienced sudden shift from oligotrophic to eutrophic state. This phenomenon is due to excess phosphorus in lake. The model describes the dynamics of phosphorus concentration and the phophorus inputs.The issue is to determine wether it is possible to maintain the lake in an oligotrophic state while preserving agricultural activities in its watershed.',
-            'statedimension':2,
-            'statenameandabbreviation':'Phosphorus Inputs,L/Phosphorus Concentration,P',
-            'controldimension':1,
-            'controlnameandabbreviation':'Phosphorus Inputs Variation,u',
-            'dynamicsdescription':'u,-b*P+L+r*P^q/(m^q+P^q)',
-            'admissiblecontroldescription':'u>=unin,u<=unax',
-            'dynamicsparameters':'b,r,q,m,unin,unax',
-            'stateconstraintdescription':'L>=Lmin,L<=Lmax,P>=0,P<=Pmax',
-            'stateconstraintparameters':'Lmin,Lmax,Pmax'
-        },{
-            'category':None,
-            'title':'Language competition',
-            'issue':'Many languages might become extinct. It is, therefore, an important challenge to understand language dynamics, and to recognise whether there are measures that can help us preserve some of them.',
-            'statedimension':3,
-            'statenameandabbreviation':'Proportion of speakers of language A,a/Proportion of speakers of language B,b/Relative prestige,s',
-            'controldimension':1,
-            'controlnameandabbreviation':'Prestige Variation,u',
-            'dynamicsdescription':"x'=(1-x-y)(1-y)^c*z-xy^c*(1-z),y'=(1-x-y)(1-x)^c*(1-z)-yx^c*z,z'=u",
-            'admissiblecontroldescription':'u in [umin;umax]',
-            'dynamicsparameters':'c,umin,umax',
-            'stateconstraintdescription':'x in [amin; 1],y in [bmin;1],z in [0;1],x+y in [0;1]',
-            'stateconstraintparameters':'amin,bmin',
-        },{
             'category':None,
             'title':'Rangeland management',
             'issue':'We consider the issue of rangeland management and use a model to describe grass dynamics consisting of two parts, the crown and the shoots. We associate the grazing pressure with rangeland management policies by adjusting stock rate. However, pastoralists can not adjust stocking rates instantaneously. The aim is to design effective policies for delivering economically and environmentally viable agricultural systems.',
@@ -60,20 +81,4 @@ class Command(BaseCommand):
             except ValidationError as e:
                 self.stdout.write("Viability problem '{}' can't be added because of these field's errors: {}".format(vp['title'],e))
             o.save()
-        
-        for software in [{
-            'name':'Patrick Saint-Pierre Software',
-            'author':'Patrick Saint-Pierre',
-            'publication':'Saint-Pierre (1994) Approximation of the viability kernel. Applied Mathematics and Optimization, 29, 187-209'
             
-        },{
-            'name':'Kd-Tree Software',
-            'author':'Isabelle Alvarez and Romain Reuillon',
-            'softwareparameters':'Exploration domain, control step, maximal division deepness, dilatation parameter, Runge-Kutta integration step, Runge-Kutta time step, random seed'
-        }]:
-            o = Algorithm(**software)
-            try:
-                o.full_clean()
-            except ValidationError as e:
-                self.stdout.write("Software '{}' can't be added because of these field's errors: {}".format(software['name'],e))
-            o.save()
